56243a80cb2c2a0d74f447d3772304c1af88dc2d
droneal11015 <droneal11015@gmail.com>
2019-09-29 23:40:45 -0400

    started landing page component

---
+++ arcade-finder/src/components/LandingPage/landingpage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Arcade Finder!</p><br/>
+                <p>Under Construction</p>
+            </div>
+        )
+    }
+}
+
+export default Landing;
+
+
